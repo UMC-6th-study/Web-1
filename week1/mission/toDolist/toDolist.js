429fd11843c5c4966c0a175148d1123afef4ee17
goodbyebada <goodbyebada@naver.com>
2024-04-09 11:57:49 +0900

    공백시 등록  금지 기능 추가

--- week1/mission/toDolist/toDolist.js
+++ week1/mission/toDolist/toDolist.js
@@ -1,25 +1,26 @@
 const todoInput = document.querySelector(".todo-form input");
 const toDoList = document.querySelector(".todo#before");
 const finishedList = document.querySelector(".todo#after");
-let toDos = [];
-let finished = [];
 
-console.log(toDoList);
+/**
+ * 이후 보충할 점s
+ * - DB or Local Storage에 저장해 새로고침 이후에도 목록이 남아있게하기
+ * - mediaquery 사용해 모바일용 화면 설정
+ */
 
 todoInput.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    setNewToDo();
+  if (event.keyCode === 13 && noGap(this)) {
+    setNewToDo(event.target.value);
+    event.target.value = " ";
   }
 });
 
-function setNewToDo() {
-  const newToDo = todoInput.value;
-  todoInput.value = " ";
-  paintToDo(newToDo);
+function setNewToDo(todoInput) {
+  const newTodo = todoInput;
+
+  paintToDo(newTodo);
 }
 
-// 배열에서 지우고 삭제하는거
 function deleteToDo(event, wasToDo) {
   const list = event.target.parentElement;
   list.remove();
@@ -36,27 +37,42 @@ function makeObj(text) {
   const span = document.createElement("span");
   const btn = document.createElement("button");
   span.innerText = text;
-  li.append(span);
+  li.appendChild(span);
   li.appendChild(btn);
   btn.type = "button";
 
   return li;
 }
 
+/**
+ * toDoInput을 해야할 일 list에 append 한다.
+ */
 function paintToDo(text) {
   const obj = makeObj(text);
   const btn = obj.querySelector("button");
   btn.innerText = "완료";
   btn.addEventListener("click", (e) => deleteToDo(e, true));
 
-  console.log(obj);
   toDoList.appendChild(obj);
 }
-
+/**
+ * 완료한 toDo text를 해낸 일 list에 append 한다.
+ */
 function paintToFinished(text) {
   const obj = makeObj(text);
   const btn = obj.querySelector("button");
   btn.innerText = "삭제";
-  btn.addEventListener("click", deleteToDo);
+  btn.addEventListener("click", (e) => deleteToDo(e, false));
   finishedList.appendChild(obj);
+}
+
+// 공백
+function noGap(obj) {
+  const str = /^\s+$/g;
+
+  if (obj.value === "" || str.exec(obj.value)) {
+    console.log("there is Gap");
+    return false;
+  }
+  return true;
 }
